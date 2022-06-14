18958f08800bffc7485cf547e1cabcf2890b32ec
Paul <fled@tuta.io>
2022-06-14 23:51:15 +0200

    Add files via upload

--- alis.py
+++ alis.py
@@ -11,6 +11,53 @@ from PyQt5.QtCore import QSize
 from datetime import date
 
 
+# Content File
+IntPython = '''# Import Libraries
+import json
+import time
+import os
+
+
+# 'Alis' Class
+class Alis:
+    def Run(self):
+        # Load Configuration File
+        ConfigJson = open('alis.json', 'r+')
+        Config = json.load(ConfigJson)
+        Updated = Config['updated']
+
+        # Libraries Installed?
+        if Updated == False:
+            time.sleep(1)
+            print()
+            Libs = Config['libs'].split(' ')
+            CurrentLib = -1
+
+            # Install Libraries
+            for i in range(len(Libs)):
+                CurrentLib = CurrentLib + 1
+                os.system('python3 -m pip install ' + Libs[CurrentLib])
+
+            # Updated = True
+            LibsRaw = Config['libs']
+            SuccessMsg = Config['success_msg']
+            RunPy = Config['run_py']
+            UpdatedTrue = {'updated': True, 'libs': LibsRaw, 'success_msg': SuccessMsg, 'run_py': RunPy}
+
+            # Delete Configuration
+            ConfigJson.seek(0)
+            ConfigJson.truncate()
+
+            # Dump Configuration
+            json.dump(UpdatedTrue, ConfigJson, indent = 2)
+            print(SuccessMsg)
+
+
+# Run & Define Alis
+Alis = Alis()
+Alis.Run()'''
+
+
 # Main Windows
 class AppWindow(QMainWindow):
     def __init__(self):
@@ -157,7 +204,6 @@ class AppWindow(QMainWindow):
             # Create Python File (Given Path)
             try:
                 ExtPython = open(Path + Slash + 'run.py', 'w')
-                IntPython = open('content.py', 'r')
                 # Copy Data
                 for Data in IntPython:
                     ExtPython.write(Data)
